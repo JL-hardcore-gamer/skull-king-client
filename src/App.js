850b0cd344688deadb00c4f929b2d1bc945c79e1
Patrick Ear <patrick.ear@scality.com>
2020-04-19 02:58:07 +0200

    Test of state

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import './App.css';
 import { Stage, Sprite } from '@inlet/react-pixi';
 import * as Colyseus from 'colyseus.js';
 
-var client = new Colyseus.Client('ws://localhost:2567');
+const client = new Colyseus.Client('ws://localhost:2567');
 
 const App = () => {
   const [room, setRoom] = useState(null);
   const [scale, setScale] = useState({ x: 1, y: 1 });
   useEffect(() => {
-    console.log('skullking', client);
-
     client
-      .create('skullking')
+      .create('arena')
       .then((room) => {
         console.log('joined successfully', room);
+        room.onStateChange.once((state) => {
+          console.log('this is the first room state!', state);
+        });
+
         setRoom(room);
         room.send({ type: 'PLAY_CARD', test: 'toto' });
       })
@@ -28,7 +30,6 @@ const App = () => {
     <Stage width={1600}>
       <Sprite
         image="https://s3-us-west-2.amazonaws.com/s.cdpn.io/693612/coin.png"
-        // image="bunny.png"
         x={100}
         y={100}
         scale={scale}
